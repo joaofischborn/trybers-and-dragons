278c2404293a28f22f5779c080657b7628f567ef
João Vitor Fischborn <joaovitorfischborn@icloud.com>
2022-11-21 15:44:23 -0300

    Requisito 05 ok

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Necromancer extends Archetype {
+  private _energyType: EnergyType;
+  private static instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+    Necromancer.instances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Necromancer.instances;
+  }
+}
+
+export default Necromancer;
